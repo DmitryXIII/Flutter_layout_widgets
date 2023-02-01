532ced55552130e43e89522e6b9c0c1bbbf1c880
Dmitry Gusev <dmtrxiii@gmail.com>
2023-02-01 17:50:09 +0300

    добавлен stateless главный экран, чтобы работал HotReload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:layout_widgets/layout_widgets_provider.dart';
 
+/// Для просмотра примеров необходимо выбрать в [_LayoutWidgetsExamples.build]
+/// виждет [LayoutWidgetsProvider]
+
 void main() {
-  runApp(LayoutWidgetsProvider.align);
+  runApp(const _LayoutWidgetsExamples());
+}
+
+class _LayoutWidgetsExamples extends StatelessWidget {
+  const _LayoutWidgetsExamples({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutWidgetsProvider.expandedColumnWithFlex;
+  }
 }
