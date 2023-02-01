c8dad77bef01739ad07fc907a8d269091dca08cb
Dmitry Gusev <dmtrxiii@gmail.com>
2023-02-01 13:40:34 +0300

    добавлен пример виджета Aling

---
+++ lib/layout_widgets_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:layout_widgets/align/align.dart';
+import 'package:layout_widgets/align/align_fractional_offset.dart';
+
+class LayoutWidgetsProvider {
+  static Widget align = getAlign();
+  static Widget alignFractionalOffset = getAlignFractionalOffset();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
-void main() {
+import 'package:flutter/material.dart';
+import 'package:layout_widgets/layout_widgets_provider.dart';
 
+void main() {
+  runApp(LayoutWidgetsProvider.alignFractionalOffset);
 }
