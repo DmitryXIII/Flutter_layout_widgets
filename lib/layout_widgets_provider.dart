24c675c5e2ed9ec884822c6c209d0480b558a3fe
Dmitry Gusev <dmtrxiii@gmail.com>
2023-02-01 13:51:57 +0300

    добавлен пример виджета Center

--- lib/layout_widgets_provider.dart
+++ lib/layout_widgets_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:layout_widgets/align/align.dart';
 import 'package:layout_widgets/align/align_fractional_offset.dart';
+import 'package:layout_widgets/center/center.dart';
 
 class LayoutWidgetsProvider {
   static Widget align = getAlign();
   static Widget alignFractionalOffset = getAlignFractionalOffset();
+  static Widget center = getCenter();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:layout_widgets/layout_widgets_provider.dart';
 
 void main() {
-  runApp(LayoutWidgetsProvider.alignFractionalOffset);
+  runApp(LayoutWidgetsProvider.align);
 }
